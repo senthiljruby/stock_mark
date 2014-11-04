8f9bd244f2b3b7534747fa56ae7f063f207a6454
Senthil Kumar <senthil@linuxmint.linuxmint-domain>
2014-11-04 01:36:45 -0500

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.min.js
+//= require jquery-ui-1.10.1.custom.min.js
+//= require breakpoints.js
+//= require jquery.unveil.min.js
+//= require jquery.bootstrap.wizard.min.js
+//= require ios7-switch
+//= require jquery.ui.touch-punch.min
+//= require modernizr
+//= require_tree .
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockMarket::Application.config.secret_token = '9496d669784bf2bb0ce47d718326729b88cdaaa336b16142f090e0140a1cfe736b7c2b1ec3f4ecc1b38dcf4ae0da3f4b9e6ebc7dabd2e6b82e7eb12b9bed83cf'
